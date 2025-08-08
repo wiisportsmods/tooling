c44e7ffbe6acdc304f26bcb325427c896c21e9b5
Personal <oss@getaddrinfo.com>
2025-08-08 14:21:53 +0100

    chore(u8/string_table): cleaner copy method

--- src/lib/formats/u8/string_table.cc
+++ src/lib/formats/u8/string_table.cc
@@ -1,18 +1,12 @@
 #include "string_table.hh"
 
 std::string string_table::copy(const int32_t offset) const {
-  size_t i = 0;
   std::ostringstream stream;
-
-  while(true) {
-    char c = _reader.read<char>(_base_offset + offset + i);
-
-    if (c == '\0') {
-      break;
-    }
-
+  char c;
+  size_t i = _base_offset + offset;
+  
+  while ((c = _reader.read<char>(i++)) != '\0') {
     stream << c;
-    i++;
   }
 
   return stream.str();
